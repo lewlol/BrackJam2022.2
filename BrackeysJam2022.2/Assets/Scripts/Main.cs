f284c8b2aa71c3bb6599ee97b32199ab17f49f9e
iArctix <connortodd.ct@gmail.com>
2022-08-23 14:26:11 +0100

    main menu tingas
    adsf

--- BrackeysJam2022.2/Assets/Scripts/Main.cs
+++ BrackeysJam2022.2/Assets/Scripts/Main.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    public GameObject button;
     
     // Start is called before the first frame update
     private void OnMouseEnter()
     {
-        
+        button.GetComponent<Spin>().enabled = true;
+        button.transform.localScale = new Vector3(1.1f, 1.1f);
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnMouseExit()
     {
-        
+        button.GetComponent<Spin>().enabled = false;
+        button.transform.localScale = new Vector3(1.0f, 1.0f);
     }
 }
