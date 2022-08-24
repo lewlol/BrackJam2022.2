998a6e191462c4498eb7bf5e43ffd6b6f72e2869
iArctix <connortodd.ct@gmail.com>
2022-08-24 14:49:41 +0100

    randomness
    vendortings

--- BrackeysJam2022.2/Assets/Scripts/Vendor.cs
+++ BrackeysJam2022.2/Assets/Scripts/Vendor.cs
@@ -10,7 +10,20 @@ public class Vendor : MonoBehaviour
     public Canvas vendorui;
     public string[] firstname;
     public string[] lastname;
+    public string[] planetname;
     public Text alienname;
+    public Text planetdescription;
+
+
+    private void Awake()
+    {
+        int fName = Random.Range(0, firstname.Length);
+        int lName = Random.Range(0, lastname.Length);
+        int pname = Random.Range(0, planetname.Length);
+
+        alienname.text = firstname[fName] + " " + lastname[lName];
+        planetdescription.text = "Welcome to " + planetname[pname];
+    }
 
     private void OnTriggerStay2D(Collider2D other)
     {
@@ -19,11 +32,9 @@ public class Vendor : MonoBehaviour
             player.constraints = RigidbodyConstraints2D.FreezeAll;
             Debug.Log("clicked");
             stats.enabled = false;
+            planetdescription.text = "Welcome to " + planetname[pname];
 
-            int fName = Random.Range(0, firstname.Length);
-            int lName = Random.Range(0, lastname.Length);
 
-            alienname.text = firstname[fName] + " " + lastname[lName];
             vendorui.enabled = true;
         }
 
