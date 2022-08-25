efa0795f6b3fd655f17cb9365b78981e7ccddda8
lewlol <lewdevbusiness@gmail.com>
2022-08-25 15:26:07 +0100

    VendorTradeUI

--- BrackeysJam2022.2/Assets/Scripts/Upgrades.cs
+++ BrackeysJam2022.2/Assets/Scripts/Upgrades.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Upgrade")]
 public class Upgrades : ScriptableObject
 {
+    public float upgradeInt;
     public string upgradeName;
-
+    public float cost;
+    public float value;
 }
