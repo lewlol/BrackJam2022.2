bf56f3c513c1a5885bf8acacdfee8ff97d99206a
lewlol <lewdevbusiness@gmail.com>
2022-08-26 18:50:26 +0100

    menumusicarrow

--- BrackeysJam2022.2/Assets/Scripts/Targetindicator.cs
+++ BrackeysJam2022.2/Assets/Scripts/Targetindicator.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Targetindicator : MonoBehaviour
 {
     public Transform Target;
+    public GameObject arrow;
 
     private void Update()
     {
         var dir = Target.position - transform.position;
 
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        arrow.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
     }
 
 }
