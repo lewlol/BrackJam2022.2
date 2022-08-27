0712aa2751962dd3a5338585d419a59f87fb5fb8
lewlol <lewdevbusiness@gmail.com>
2022-08-27 13:44:50 +0100

    poooge

--- BrackeysJam2022.2/Assets/Scripts/RenderObjects.cs
+++ BrackeysJam2022.2/Assets/Scripts/RenderObjects.cs
@@ -7,22 +7,6 @@ public class RenderObjects : UnityEngine.MonoBehaviour
     public List <GameObject> objects;
     [SerializeField] private GameObject player;
     public Camera cam;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag == "Planet" || collision.gameObject.tag == "Asteroid")
-        {
-            collision.gameObject.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Planet" || collision.gameObject.tag == "Asteroid")
-        {
-            collision.gameObject.SetActive(false);
-        }
-    }
     void RefreshObjects()
     {
         foreach(var obj in objects)
